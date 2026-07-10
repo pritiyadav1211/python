b99aa149eabd1b9c15c19160931dd80a4407ed10
pritiyadav1211 <pyadav12112006@gmail.com>
2026-07-10 13:03:30 +0530

    CHATGPT TEST

---
+++ test.py
@@ -0,0 +1,116 @@
+# python={"priti","jiya","nara"}
+# sql={"deepika","muskan","priti","nara"}
+# print(python.intersection(sql)) 
+# student={
+#     "student1":{
+#         "name":"priti",
+#         "subject":"ai",
+#         "emailid":"abc@gmail.com",
+#     },
+#     "student2":{  
+#             "name":"deepika",
+#             "subject":"web development", 
+#             "emailid":"bcd@gmail.com",
+#         }
+        
+# }
+# print(student)  
+# print(student["student2"].get("age","not available"))
+
+# list=[2,3,45]
+# list.append(5)
+# print(list)
+# list.extend("67")
+# print(list)
+
+# fruits=["apple","litchi","orange","strawberry","banana"]
+# print(fruits)
+# fruits.append("mango")
+# print(fruits)
+# fruits.insert(2,"orange")
+# print(fruits)
+# fruits.remove("apple")
+# print(fruits)
+# list=[2,5,7,8]
+# print(max(list))
+
+# #tuple question
+# num=(10,30,4,10,6)
+# print(num)
+# print(num.count(10))
+# print(num.index(30))
+
+# #set question
+# set1={1,2,3,4}
+# set2={4,5,6,7}
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+# print(set1.difference(set2))
+# set1.remove(4)
+# print(set1)
+# set1.discard(2)
+# print(set1)
+
+# #dictionary que
+# dict={
+#     "name":"priti",
+#     "age":18,
+#     "emailid":"abc@gmail.com"
+# }
+# print(dict)
+# dict["age"]=19
+# print(dict)
+# print(dict.keys())
+# print(dict.items())
+# for i in dict.items():
+#     print(i)
+
+# name="priti"
+# age=18
+# city="nahar"
+# percentage= 85.5
+# print(name)
+
+# a=10
+# b=20
+# a,b=b,a
+# print(a,b)
+# #to calculate the area of rectangle using variables
+# a=10
+# b=20
+# area=a*b
+# print(area)
+
+#create a string
+text="priti"
+print(len(text))
+print(text[0])
+print(text[4])
+print(text.upper())
+print(text.lower())
+
+#list 
+list=["apple","mango","litchi","kiwi","cherry"]
+list.append("strawberry")
+print(list)
+list.insert(3,"guvava")
+print(list)
+# list.extend("papaya")
+# print(list)
+# list.remove("cherry")
+# print(list)
+list.pop()
+print(list)
+#list.clear()
+#print(list)
+
+#tuple
+num=(2,3,4,5,3,7)
+print(max(num))
+print(min(num))
+print(len(num))
+print(num.count(3))
+print(num.index(5))
+
+
+
